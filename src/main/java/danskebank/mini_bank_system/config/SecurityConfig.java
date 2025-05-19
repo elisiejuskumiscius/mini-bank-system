83633628395a8faca996b325f44336a24b4038d8
elisiejuskumiscius <ekumiscius@gmail.com>
2025-05-19 20:00:04 +0300

    added actuator

---
+++ src/main/java/danskebank/mini_bank_system/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package danskebank.mini_bank_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+}
