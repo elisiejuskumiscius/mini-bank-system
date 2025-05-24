eefaf2b1bd186428b4cc2092695516fb114d9f7c
elisiejuskumiscius <ekumiscius@gmail.com>
2025-05-24 20:31:48 +0300

    terraform destroy action added

--- terraform/main.tf
+++ terraform/main.tf
@@ -199,3 +199,27 @@ resource "aws_s3_object" "upload_outputs" {
   key    = "outputs.json"
   source = local_file.output_file.filename
 }
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "elisiejus-app-tf-state-bucket"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "terraform-locks"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "TerraformLockTable"
+  }
+}
